94dd8a1e959cc6ee7294d136998d17c9e9b16fc8
nadeen-maree <nadeen.maree1610@gmail.com>
2023-12-03 17:08:30 +0200

    added JSON and Handlebars exercises, and Minor code improvements

--- week-2/callbacks-and-arrow-functions/arrow-functions/exercise5/main.js
+++ week-2/callbacks-and-arrow-functions/arrow-functions/exercise5/main.js
@@ -1,7 +1,6 @@
 
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 
-
 console.log(capitalize("bOb"))
 console.log(capitalize("TAYLOR"))
 console.log(capitalize("feliSHIA"))
---
+++ week-4/JSON/Question3+4/main.js
@@ -0,0 +1,14 @@
+let animals = {
+  "cow":{
+    "name": "Cow",
+    "weight":{
+      "value": 600,
+      "unit": "kilograms"
+    },
+    "habitat": ["farm", "grassland"],
+    "endangered": false,
+    "food": ["grass", "hay", "grains", "silage"],
+    "predators": [ "wolves", "big cats"],
+    "deliciousnessRating": 4
+  }
+}
